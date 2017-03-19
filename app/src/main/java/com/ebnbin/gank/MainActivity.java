460cc125b6f150089033f7180b8f40a525d53bfb
Ebn Zhang <ebnbin@gmail.com>
2017-03-19 23:13:15 +0800

    Use OkHttp to get a url and toast the response

--- app/src/main/java/com/ebnbin/gank/MainActivity.java
+++ app/src/main/java/com/ebnbin/gank/MainActivity.java
@@ -1,6 +1,47 @@
 package com.ebnbin.gank;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mHandler = new Handler();
+
+        String url = "http://gank.io/api/day/2015/05/18";
+        Request request = new Request.Builder().url(url).build();
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseString = response.body().string();
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, responseString, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
 }
