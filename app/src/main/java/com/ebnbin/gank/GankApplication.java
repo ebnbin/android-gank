0ffaf60dc2bd81b36d3299526b75e7d5e09ed01e
Ebn Zhang <ebnbin@gmail.com>
2017-03-21 16:40:52 +0800

    Update library dependency 'com.ebnbin:eb:0.0.4' and remove redundant library dependencies 'com.google.code.gson:gson:2.8.0' and 'com.squareup.okhttp3:okhttp:3.6.0'

---
+++ app/src/main/java/com/ebnbin/gank/GankApplication.java
@@ -0,0 +1,11 @@
+package com.ebnbin.gank;
+
+import android.app.Application;
+
+import com.ebnbin.eb.base.EBApplication;
+
+/**
+ * {@link Application} class.
+ */
+public final class GankApplication extends EBApplication {
+}
