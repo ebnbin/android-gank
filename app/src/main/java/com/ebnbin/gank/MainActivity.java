a050c928f2a9dc6a70e34a872a8e8dd18a877c19
Ebn Zhang <ebnbin@gmail.com>
2017-03-27 23:05:19 +0800

    Add feature: About

--- app/src/main/java/com/ebnbin/gank/MainActivity.java
+++ app/src/main/java/com/ebnbin/gank/MainActivity.java
@@ -3,6 +3,9 @@ package com.ebnbin.gank;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ebnbin.ebapplication.base.EBActivity;
 import com.ebnbin.gank.feature.days.DaysFragment;
@@ -18,6 +21,27 @@ public final class MainActivity extends EBActivity {
             daysFragment = new DaysFragment();
             getSupportFragmentManager().beginTransaction()
                     .replace(android.R.id.content, daysFragment, DaysFragment.class.getName()).commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about: {
+                Toast.makeText(getContext(), R.string.main_about_content, Toast.LENGTH_LONG).show();
+
+                return true;
+            }
+            default: {
+                return super.onOptionsItemSelected(item);
+            }
         }
     }
 }
