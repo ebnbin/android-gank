e340b9ce8ea68928bb2f53ec620671a9e0eb3595
Ebn Zhang <ebnbin@gmail.com>
2017-04-07 02:32:57 +0800

    Enable EBUtil debug mode

--- app/src/main/java/com/ebnbin/gank/GankApplication.java
+++ app/src/main/java/com/ebnbin/gank/GankApplication.java
@@ -2,10 +2,21 @@ package com.ebnbin.gank;
 
 import android.app.Application;
 
+import com.ebnbin.eb.util.EBUtil;
 import com.ebnbin.ebapplication.context.EBApplication;
 
 /**
  * {@link Application} class.
  */
 public final class GankApplication extends EBApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initEBUtil();
+    }
+
+    private void initEBUtil() {
+        EBUtil.debug = BuildConfig.DEBUG;
+    }
 }
