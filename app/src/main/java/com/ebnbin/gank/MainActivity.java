1f7e832d774a1bc5f7246e33dc2dcfb17d933d9b
Ebn Zhang <ebnbin@gmail.com>
2017-04-14 13:41:12 +0800

    Move options menu item 'about' from activity level to fragment level

--- app/src/main/java/com/ebnbin/gank/MainActivity.java
+++ app/src/main/java/com/ebnbin/gank/MainActivity.java
@@ -3,9 +3,6 @@ package com.ebnbin.gank;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.ebnbin.ebapplication.context.ui.EBActivity;
 import com.ebnbin.gank.feature.days.DaysFragment;
@@ -23,27 +20,6 @@ public final class MainActivity extends EBActivity {
                     .add(daysFragment)
                     .endTransaction();
             ft.commit();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_activity, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.about: {
-                Toast.makeText(getContext(), R.string.main_about_content, Toast.LENGTH_LONG).show();
-
-                return true;
-            }
-            default: {
-                return super.onOptionsItemSelected(item);
-            }
         }
     }
 }
--- app/src/main/java/com/ebnbin/gank/feature/days/DaysFragment.java
+++ app/src/main/java/com/ebnbin/gank/feature/days/DaysFragment.java
@@ -1,11 +1,17 @@
 package com.ebnbin.gank.feature.days;
 
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.ebnbin.ebapplication.context.ui.EBActionBarFragment;
+import com.ebnbin.gank.R;
 import com.ebnbin.gank.feature.daysviewpager.DaysViewPagerFragment;
 
 public final class DaysFragment extends EBActionBarFragment {
@@ -21,5 +27,32 @@ public final class DaysFragment extends EBActionBarFragment {
                     .add(COORDINATOR_LAYOUT_CONTENT_CONTAINER_ID, daysViewPagerFragment);
             ft.commit();
         }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.fragment_days, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about: {
+                Toast.makeText(getContext(), R.string.days_about_content, Toast.LENGTH_LONG).show();
+
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
