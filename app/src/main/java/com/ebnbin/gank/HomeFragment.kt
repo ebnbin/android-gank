815c0544657946655500ef28b0b6f7bd71f0a5b0
Ebn Zhang <ebnbin@gmail.com>
2017-07-03 11:40:36 +0800

    Use fragment helper to manage fragments of bottom navigation instead of viewPager

--- app/src/main/java/com/ebnbin/gank/HomeFragment.kt
+++ app/src/main/java/com/ebnbin/gank/HomeFragment.kt
@@ -3,12 +3,23 @@ package com.ebnbin.gank
 import android.os.Bundle
 import android.view.View
 import com.ebnbin.ebapplication.context.EBBottomNavigationFragment
+import com.ebnbin.ebapplication.context.EBBottomNavigationItem
+import com.ebnbin.ebapplication.feature.webview.WebViewActionBarFragment
+import com.ebnbin.gank.feature.category.CategoryFragment
+import com.ebnbin.gank.feature.days.DaysFragment
 
 class HomeFragment : EBBottomNavigationFragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        bottomNavigationViewPager.adapter = HomePagerAdapter(childFragmentManager)
-        bottomNavigation.setupWithViewPager(bottomNavigationViewPager)
+        val daysFragment = DaysFragment()
+        bottomNavigation.addItem(EBBottomNavigationItem(getString(R.string.home), R.drawable.home, daysFragment))
+        val categoryFragment = CategoryFragment()
+        bottomNavigation.addItem(EBBottomNavigationItem("Category", R.drawable.home_subject, categoryFragment))
+        val webViewActionBarFragment = WebViewActionBarFragment.newInstance("http://ebnbin.com")
+        bottomNavigation.addItem(EBBottomNavigationItem(getString(R.string.eb_ebnbin), R.drawable.home_person,
+                webViewActionBarFragment))
+
+        bottomNavigation.setCurrentItem(0, true)
     }
 }
